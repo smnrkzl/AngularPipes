7f229c67ab1c408130a8027d3c7309fb016803fa
smnrkzl <kizilsemanur@gmail.com>
2023-01-19 16:48:13 +0300

    Pipes

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'pipes';
+  name = '';
+  date = '';
+  money: number = 0;
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onMoneyChange(value: string) {
+    this.money = parseFloat(value);
+  }
+}
